664c94ad747fad569089d16efd5ee1d402a2d560
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-05-18 22:30:32 -0700

    first commit

---
+++ AspNetCoreScraper/Models/StockViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreScraper.Models
+{
+    public class StockViewModel
+    {
+        public Stock[] Stocks { get; set; }
+    }
+}
