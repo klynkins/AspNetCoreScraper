3668fd40107cd2a7d39870968d14a1f38f801efd
Katelynn Campbell-Bishop <klyncamp23@gmail.com>
2019-05-28 08:01:16 -0700

    admin

--- AspNetCoreScraper/SeedData.cs
+++ AspNetCoreScraper/SeedData.cs
@@ -19,17 +19,36 @@ namespace AspNetCoreScraper
 
             var userManager = services
                  .GetRequiredService<UserManager<IdentityUser>>();
-            //await EnsureTestAdminAsync(userManager);
+            await EnsureTestAdminAsync(userManager);
         }
 
         private static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
         {
             var alreadyExists = await roleManager
-                //.RoleExistsAsync(IdentityConstants.AdministratorRole);
+                .RoleExistsAsync(IdentityConstants.AdministratorRole);
 
             if (alreadyExists) return;
 
             await roleManager.CreateAsync(new IdentityRole(IdentityConstants.AdministratorRole));
+        }
+
+        private static sync Task EnsureTestAdminAsync(UserManager<IdentityUser> userManager)
+        {
+            var testAdmin = await userManager.Users
+                .Where(x => x.UserName == "admin@todo.local")
+                .SingleOrDefaultAsync();
+
+            if (testAdmin != null) return;
+
+            testAdmin = new IdentityUser
+            {
+                UserName = "admin@todo.local",
+                Email = "admin@todo.local"
+            };
+            await userManager.CreateAsync(
+                testAdmin, "NotSecure123");
+            await userManager.AddToRoleAsync(
+                testAdmin, Constants.AdministratorRole);
         }
     }
 }
